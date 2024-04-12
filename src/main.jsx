abb16fd6b9caebab4d2be2148235ea4b3131bfb5
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-04-12 23:43:23 +0600

    added mui package

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
+
 import router from "./routes/Router.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
