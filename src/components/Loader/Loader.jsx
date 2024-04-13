e0effa080f89256c7ff5063cd3d2dd94afadc1b3
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-04-13 11:38:48 +0600

    added loader

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+import { ClockLoader } from "react-spinners";
+
+const Loader = () => {
+  return (
+    <div
+      className={`min-h-[calc(100vh-80px)] flex flex-col  justify-center  items-center `}
+    >
+      <ClockLoader color="#36d7b7" />
+    </div>
+  );
+};
+
+export default Loader;
--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -1,7 +1,12 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
+import Loader from "../../components/Loader/Loader";
 
 const MainLayout = () => {
+  const navigation = useNavigation();
+
+  if (navigation.state === "loading") return <Loader />;
+
   return (
     <div>
       <div className="w-full h-20">
