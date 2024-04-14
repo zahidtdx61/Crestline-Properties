42543ea3f1fdc4bccfb4e809e4f4e0535154065d
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-04-14 21:22:58 +0600

    fixed user auth issue

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -2,10 +2,13 @@ import { Navigate, useLocation } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 
 import PropTypes from "prop-types";
+import Loader from "../components/Loader/Loader";
 
 const PrivateRoutes = ({ children }) => {
-  const { user } = useAuth();
+  const { user, isLoading } = useAuth();
   const location = useLocation();
+
+  if (isLoading) {return <Loader />}
 
   if (!user) {
     return <Navigate to="/sign-in" state={location.pathname || "/"} />;
